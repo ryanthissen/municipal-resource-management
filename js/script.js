3f7e719a9f277963a012b493f95c6e2690ac8602
Ryan Thissen <rmt1855@gmail.com>
2017-02-07 10:42:39 -0800

    added work

--- js/script.js
+++ js/script.js
@@ -1,54 +1,79 @@
 $(document).ready(function() {
     $('select').material_select();
     $('.parallax').parallax();
-})
-// function myMap() {
-//   let mapCanvas = document.getElementsByClassName('map')[0];
-//   let mapOptions = {
-//     center: new google.maps.LatLng(37.871233, -122.267303),
-//     zoom: 13
-//   };
-//   let map = new google.maps.Map(mapCanvas, mapOptions)
-// }
+  });
 let objArr = [];
-let urlGenerator = document.getElementsByClassName('url-form')[0];
+let jsonForm = document.getElementById('url-form');
 let flexContainer = document.getElementById('flex-container');
+let searchedResource = [];
 
-
-urlGenerator.addEventListener('submit', function(event) {
+jsonForm.addEventListener('submit', function(event) {
   event.preventDefault();
   const url = `https://data.cityofberkeley.info/resource/fe5b-yv79.json?$where=${document.getElementById('select-resource').value} > 0&department=${document.getElementById('select-dept').value}`;
-  // console.log(document.getElementById('select-dept').value);
-  // console.log(document.getElementById('select-resource').value);
-  // console.log(url);
+  console.log(url);
+  searchedResource.push(document.getElementById('select-resource').value);
   fetch(url)
     .then(function(pObj) {
       return pObj.json();
     })
     .then(function(jsonObj) {
       for (let i = 0; i < jsonObj.length; i++) {
-        let newObj = {}
-          newObj.site_name = jsonObj[i].site;
-          newObj.department = jsonObj[i].department;
-          newObj.end_use = jsonObj[i].end_use;
-          newObj.address = jsonObj[i].location_1_address;
-          newObj.city = jsonObj[i].location_1_city;
-          newObj.state = jsonObj[i].location_1_state;
-          newObj.year = jsonObj[i].year;
-          newObj.kwh = jsonObj[i].annual_energy_kwheq;
-          newObj.gallons = jsonObj[i].annual_water_gallons;
-          newObj.various_location = "";
-          if (jsonObj[i].location_1) {
-            newObj.latitude = jsonObj[i].location_1.coordinates[0];
-            newObj.longitude = jsonObj[i].location_1.coordinates[1];
-          } else {
-            newObj.various_location = "Various, throughout city.";
-          }
+        if (searchedResource[0] === "annual_elec_kwh") {
+          let newObj = {};
+            newObj.site_name = jsonObj[i].site;
+            newObj.department = jsonObj[i].department;
+            newObj.end_use = jsonObj[i].end_use;
+            newObj.address = jsonObj[i].location_1_address;
+            newObj.city = jsonObj[i].location_1_city;
+            newObj.state = jsonObj[i].location_1_state;
+            newObj.year = jsonObj[i].year;
+            newObj.kwh = jsonObj[i].annual_energy_kwheq;
+            newObj.various_location = "";
+            if (jsonObj[i].location_1) {
+              newObj.latitude = jsonObj[i].location_1.coordinates[0];
+              newObj.longitude = jsonObj[i].location_1.coordinates[1];
+            } else {
+              newObj.various_location = "Various, throughout city.";
+            }
+            objArr.push(newObj);
+        } else {
+          let newObj = {};
+            newObj.site_name = jsonObj[i].site;
+            newObj.department = jsonObj[i].department;
+            newObj.end_use = jsonObj[i].end_use;
+            newObj.address = jsonObj[i].location_1_address;
+            newObj.city = jsonObj[i].location_1_city;
+            newObj.state = jsonObj[i].location_1_state;
+            newObj.year = jsonObj[i].year;
+            newObj.gallons = jsonObj[i].annual_water_gallons;
+            newObj.various_location = "";
+            if (jsonObj[i].location_1) {
+              newObj.latitude = jsonObj[i].location_1.coordinates[0];
+              newObj.longitude = jsonObj[i].location_1.coordinates[1];
+            } else {
+              newObj.various_location = "Various, throughout city.";
+            }
         objArr.push(newObj);
+        }
       }
-      console.log(objArr);
+      return objArr;
     })
     .then(function(objArr) {
-
+      if (objArr[0].gallons){
+        var sortedObjArr = objArr.slice(0);
+        sortedObjArr.sort(function(a,b) {
+          return a.gallons - b.gallons;
+        });
+        return sortedObjArr;
+      }
+      else {
+        var sortedObjArr = objArr.slice(0);
+        sortedObjArr.sort(function(a,b) {
+          return a.kwh - b.kwh;
+        });
+        return sortedObjArr;
+      }
     })
+    .then(function(sortedObjArr)
+    
 })
