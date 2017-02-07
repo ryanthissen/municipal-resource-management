678ea6274c72497e484cf57c38872f644a4521a9
Ryan Thissen <rmt1855@gmail.com>
2017-02-07 11:39:08 -0800

    added work

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
     $('select').material_select();
     $('.parallax').parallax();
+    $('.collapsible').collapsible({
+        accordion: false,
+        // onOpen: function(el) { alert('Open'); }, // Callback for Collapsible open
+        // onClose: function(el) { alert('Closed'); } // Callback for Collapsible close
+      });
   });
 let objArr = [];
 let jsonForm = document.getElementById('url-form');
 let flexContainer = document.getElementById('flex-container');
+let collapsible = document.getElementsByClassName('collapsible')[0];
 let searchedResource = [];
 
 jsonForm.addEventListener('submit', function(event) {
   event.preventDefault();
   const url = `https://data.cityofberkeley.info/resource/fe5b-yv79.json?$where=${document.getElementById('select-resource').value} > 0&department=${document.getElementById('select-dept').value}`;
-  console.log(url);
+  // console.log(url);
   searchedResource.push(document.getElementById('select-resource').value);
   fetch(url)
     .then(function(pObj) {
@@ -71,9 +77,37 @@ jsonForm.addEventListener('submit', function(event) {
         sortedObjArr.sort(function(a,b) {
           return a.kwh - b.kwh;
         });
+        console.log(sortedObjArr);
         return sortedObjArr;
       }
     })
-    .then(function(sortedObjArr)
-    
+    .then(function(sortedObjArr) {
+      for (i = 0; i < sortedObjArr.length; i++) {
+        let br = document.createElement("br")
+        let newLi = document.createElement("li");
+        let newHead = document.createElement("div");
+        newHead.setAttribute("class", "collapsible-header")
+        let newBody = document.createElement("div");
+        newBody.setAttribute("class", "collapsible-body")
+        let headContent = document.createTextNode(sortedObjArr[i].site_name)
+
+        if (sortedObjArr[0].kwh) {
+          let bodyContent1 = document.createTextNode('Address:  ' + sortedObjArr[i].address);
+          let bodyContent2 = document.createTextNode('Energy Usage:  ' + sortedObjArr[i].kwh + ' kwh');
+          let bodyContent3 = document.createTextNode('Year of use:  ' + sortedObjArr[i].year);
+          newHead.appendChild(headContent);
+          newBody.appendChild(bodyContent1);
+          newBody.appendChild(document.createElement("br"));
+          newBody.appendChild(bodyContent2);
+          newBody.appendChild(document.createElement("br"));
+          newBody.appendChild(bodyContent3);
+          newLi.appendChild(newHead);
+          newLi.appendChild(newBody);
+          collapsible.appendChild(newLi);
+
+        }
+
+      }
+  });
+
 })
